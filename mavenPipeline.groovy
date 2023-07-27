5fb6c9e72a2bbec9359d2e87d90b5d56a4bf77d5
LeongJR <23007472@myrp.edu.sg>
2023-07-27 21:31:43 +0800

    Initial Commit

---
+++ mavenPipeline.groovy
@@ -0,0 +1,57 @@
+pipeline {
+
+    agent any
+
+    stages {
+        stage("SCM Checkout"){
+            steps{
+                echo "Checking Out SCM"
+                echo "Repository Selected : "
+                gitCloneCheckOut();
+            }
+        }
+    }
+        stage("Project Build"){
+            steps{
+                echo "Commencing Build"
+                mavenBuild()
+
+            }
+        }
+
+        stage("Sonar Scan"){
+            steps{
+                echo "Commencing Sonar Scan - To be implemented"
+                //sonarScan()
+            }
+        }
+
+        post {
+            always{
+                cleanWs (cleanWhenFailure: false,
+                        cleanWhenNotBuilt: false,
+                        cleanWhenUnstable: false,
+                        notFailBuild: true)
+            }
+        }
+}
+
+
+/* FUNCTIONS */
+gitCloneCheckOut() {
+
+    checkout scmGit(branches: [[name: "${Branch}"]],
+                    extensions: [],
+                    userRemoteConfigs: [[credentialsId: '4e2b7a04-cf48-4c19-9039-31eb9693d8a7',
+                                         url: "https://github.com/LeongJR/${Repository}"]])
+}
+
+mavenBuild() {
+    sh """
+       echo 'Verify java version and compiler version'
+       java --version
+       javac --version
+       mvn -f pom.xml clean install
+
+       """
+}
