3d0e5fffe87d25b57d25c7b42529d1962d55550f
LeongJR <137897949+LeongJR@users.noreply.github.com>
2023-11-23 21:56:24 +0800

    Create CWF_PIPELINE.groovy

---
+++ CWF_PIPELINE.groovy
@@ -0,0 +1,87 @@
+pipeline {
+
+    agent any
+
+    tools { maven "Maven 3.9.3"
+            jdk "${JAVA_VERSION}"
+          }
+
+    stages {
+        stage("S1 3647096t"){
+            steps{
+                echo "Checking Out SCM"
+                echo "Repository Selected : ${Repository} "
+                gitCloneCheckOut()
+            }
+        }
+
+        stage("S2 3647096t"){
+            steps{
+                echo "Commencing Build"
+                mavenBuild()
+
+            }
+        }
+        
+        stage("S3 3647096t"){
+            steps{
+                echo "Commencing Sonar Scan"
+                sonarScan()
+            }
+        } 
+
+        stage("S4 3647096t"){
+            steps{
+            deploy adapters: [tomcat9(credentialsId: 'Tomcat', path: '', url: 'http://localhost:8090')], contextPath: "${Repository}", war: '**/*.war'
+            }
+        }     
+      
+        stage("S5 3647096t"){
+            steps{
+            deploy adapters: [tomcat9(credentialsId: 'Tomcat', path: '', url: 'http://localhost:8090')], contextPath: "${Repository}", war: '**/*.war'
+            }
+        }        
+    }
+
+        post {
+            always{
+                cleanWs (cleanWhenFailure: false,
+                        cleanWhenNotBuilt: false,
+                        cleanWhenUnstable: false,
+                        notFailBuild: true)
+            }
+        }
+}
+
+
+/* FUNCTIONS */
+def gitCloneCheckOut() {
+
+    checkout scmGit(branches: [[name: "${Branch}"]],
+                    extensions: [],
+                    userRemoteConfigs: [[credentialsId: '4e2b7a04-cf48-4c19-9039-31eb9693d8a7',
+                                         url: "https://github.com/LeongJR/${Repository}"]])
+}
+
+def mavenBuild() {
+    bat """
+       echo 'Verify java version and compiler version'
+       java --version 
+       javac --version
+       mvn -f pom.xml clean install
+       """
+}
+
+def sonarScan() {
+
+    bat """
+          mvn sonar:sonar \
+         -Dsonar.projectKey="${Repository}" \
+         -Dsonar.projectName="${Repository}" \
+         -Dsonar.sources=src/main \
+         -Dsonar.sourceEncoding=UTF-8 \
+         -Dsonar.language=java \
+         -Dsonar.test=src/test \
+         """
+      
+}
